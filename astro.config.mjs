6442b0be69288954de961aecb81bfc64756aaaa0
Rajesh Patidar <patidarchat@gmail.com>
2023-01-04 19:40:49 +0530

    updating the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rpatidar.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  base: '/myblog',
 });
